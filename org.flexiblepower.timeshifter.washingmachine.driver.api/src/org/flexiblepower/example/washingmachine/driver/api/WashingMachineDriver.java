b83813f19c822edf0cd878d422946ea358f6fac6
Mente Konsman <mente.konsman@tno.nl>
2013-10-08 11:30:04 +0200

    modified:   org.flexiblepower.example.washingmachine.driver.impl/bnd.bnd

---
+++ org.flexiblepower.timeshifter.washingmachine.driver.api/src/org/flexiblepower/example/washingmachine/driver/api/WashingMachineDriver.java
@@ -0,0 +1,7 @@
+package org.flexiblepower.example.washingmachine.driver.api;
+
+import org.flexiblepower.ral.ResourceDriver;
+
+public interface WashingMachineDriver extends ResourceDriver<WashingMachineState, WashingMachineControlParameters> {
+
+}

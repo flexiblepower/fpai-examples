deba2cab4c0b38591935fb1de5ab1818c5617917
Mente Konsman <mente.konsman@tno.nl>
2014-10-06 13:37:53 +0200

    Deleted all old example projects that only work with the 13.10 version.

--- org.flexiblepower.example.energyapp/res/widgets/ExampleWidget/script.js
+++
@@ -1,8 +0,0 @@
-$(window).load(function() {
-	w = new widget("update", 1000, function(data) {
-		$("#loading").detach();
-		$("p").show();
-		$(".error").hide();
-		$("#exampleField").text(data.exampleField);
-	});
-});
--- org.flexiblepower.example.timeshifter.washingmachine.driver.api/src/org/flexiblepower/example/timeshifter/washingmachine/driver/api/WashingMachineDriver.java
+++
@@ -1,7 +0,0 @@
-package org.flexiblepower.example.timeshifter.washingmachine.driver.api;
-
-import org.flexiblepower.ral.ResourceDriver;
-
-public interface WashingMachineDriver extends ResourceDriver<WashingMachineState, WashingMachineControlParameters> {
-
-}
